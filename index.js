84ed1b16bba24f583ac705be24c1d5b5ffe96435
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-05-04 15:30:27 +0200

    atelier

---
+++ index.js
@@ -0,0 +1,12 @@
+// const inputs = document.querySelectorAll('input')
+// const result = document.querySelector('result')
+
+
+
+// inputs.forEach(function(input){
+//     input.addEventListener('click' ,() => {
+//         console.log(input.value);
+//         result.value = input.value
+//     })
+
+// });
